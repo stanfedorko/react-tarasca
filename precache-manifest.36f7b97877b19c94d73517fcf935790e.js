b9cc16aed8ff59fe820b8738087b9c71d2a20e42
Stas <fedorkostas@gmail.com>
2021-03-06 16:02:30 +0100

    Updates

---
+++ precache-manifest.36f7b97877b19c94d73517fcf935790e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980f06464456538823c37d7dbfbee5d2",
+    "url": "/react-tarasca/index.html"
+  },
+  {
+    "revision": "dd226c5188475642a887",
+    "url": "/react-tarasca/static/css/main.30b38865.chunk.css"
+  },
+  {
+    "revision": "ec9732fcd89a30b74738",
+    "url": "/react-tarasca/static/js/2.309f3ba9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tarasca/static/js/2.309f3ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd226c5188475642a887",
+    "url": "/react-tarasca/static/js/main.10b5ad5d.chunk.js"
+  },
+  {
+    "revision": "37eca9164bbfd2d28e3b",
+    "url": "/react-tarasca/static/js/runtime-main.7265ba4a.js"
+  },
+  {
+    "revision": "f09137884fb4e8925e3dbd3e4fbe5d16",
+    "url": "/react-tarasca/static/media/about-img.f0913788.svg"
+  },
+  {
+    "revision": "42c4d0602942aa7c574714dd16b8300a",
+    "url": "/react-tarasca/static/media/card-04.42c4d060.png"
+  },
+  {
+    "revision": "3c5c934c620f1844e2ac8fb9a741a056",
+    "url": "/react-tarasca/static/media/card-05.3c5c934c.png"
+  },
+  {
+    "revision": "cd61401c290d2e87984690965e569f45",
+    "url": "/react-tarasca/static/media/card-12.cd61401c.png"
+  },
+  {
+    "revision": "efff5ad0e0bd5804789c92a061e11fd3",
+    "url": "/react-tarasca/static/media/card-17.efff5ad0.png"
+  },
+  {
+    "revision": "b7f8d4664f3f75b4abcee4d806c3f9f0",
+    "url": "/react-tarasca/static/media/card-26.b7f8d466.png"
+  },
+  {
+    "revision": "6eaaad5f348d108b20f7b050ed10045b",
+    "url": "/react-tarasca/static/media/card-29.6eaaad5f.png"
+  },
+  {
+    "revision": "e9d9fa7e54a41e1f4f2bdccad8c605be",
+    "url": "/react-tarasca/static/media/card-34.e9d9fa7e.png"
+  },
+  {
+    "revision": "0c3331a5d52b09eb3fa9bfe1461d0208",
+    "url": "/react-tarasca/static/media/card-45.0c3331a5.png"
+  },
+  {
+    "revision": "4083de863e7b508f5dd3dc8aa0ce5106",
+    "url": "/react-tarasca/static/media/card-48.4083de86.png"
+  },
+  {
+    "revision": "3b8774451fd8ff7b3e6db2b0408d8149",
+    "url": "/react-tarasca/static/media/card-50.3b877445.png"
+  },
+  {
+    "revision": "1efcfa27d08962dbe8e9c19f77ce4056",
+    "url": "/react-tarasca/static/media/circle-icon-1.1efcfa27.svg"
+  },
+  {
+    "revision": "22e3792ac823c059d8defff147c244a4",
+    "url": "/react-tarasca/static/media/circle-icon-2.22e3792a.svg"
+  },
+  {
+    "revision": "7471ba2b856ec0bf393dd0b39a1a92eb",
+    "url": "/react-tarasca/static/media/circle-icon-3.7471ba2b.svg"
+  },
+  {
+    "revision": "b6121005c1df2424afcf4f0607f33a2b",
+    "url": "/react-tarasca/static/media/danger-figure.b6121005.svg"
+  },
+  {
+    "revision": "93cfafc8cffd59215b860a2235ad202f",
+    "url": "/react-tarasca/static/media/faq-box-img.93cfafc8.png"
+  },
+  {
+    "revision": "dfcd4a926a13ea8c23a1dfee5f9918ca",
+    "url": "/react-tarasca/static/media/footer-logo.dfcd4a92.svg"
+  },
+  {
+    "revision": "63cfca88cbb78471b5ab63e4417d4d5f",
+    "url": "/react-tarasca/static/media/logo.63cfca88.svg"
+  },
+  {
+    "revision": "528956cefdafcc3c9fd2be7b894283e5",
+    "url": "/react-tarasca/static/media/primary-figure.528956ce.svg"
+  },
+  {
+    "revision": "396c0f89c6f41f52e722030c3df87aac",
+    "url": "/react-tarasca/static/media/warning-figure.396c0f89.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-tarasca/precache-manifest.36f7b97877b19c94d73517fcf935790e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tarasca/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
